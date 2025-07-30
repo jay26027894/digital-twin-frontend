5308f4a7d6bf6f4e30b427ee1c3de33c50517322
jay26027894 <125072052+jay26027894@users.noreply.github.com>
2025-07-30 15:03:08 +0530

    fixed footer date

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -257,7 +257,7 @@ function Landing() {
           </div>
           
           <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-            <p>&copy; 2024 DigiTwin. All rights reserved.</p>
+            <p>&copy; 2025 DigiTwin. All rights reserved.</p>
           </div>
         </div>
       </footer>
